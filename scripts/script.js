3fc5c93a7d1f57d00ec1a72a8fd1cd3fdc2d0e37
Alzam A <alzamakbar2510@gmail.com>
2023-04-16 21:37:02 +0700

    Revision 1.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,57 +1,40 @@
-// Memunculkan popup form
+// Variabel
 const popup = document.querySelector('.popup')
-const cls_btn = popup.querySelector('.popup__close-btn')
-const edit_btn = document.querySelector('.profile__edit-btn')
+const form = popup.querySelector('.popup__container')
+const closeButton = popup.querySelector('.popup__close-btn')
+const editButton = document.querySelector('.profile__edit-btn')
+let inputName = popup.querySelector('.popup__input_text_name')
+let inputTitle = popup.querySelector('.popup__input_text_title')
+let dName = document.querySelector('.profile__name')
+let dTitle = document.querySelector('.profile__title')
 
+// Function popup open
 function handlerPopupForm() {
+  inputName.value = dName.textContent;
+  inputTitle.value = dTitle.textContent;
+
   popup.classList.add('popup_opened');
 }
-
+// Function popup close
 function handleCloseForm(event) {
   event.preventDefault();
-
-  popup.classList.remove('popup_opened')
-}
-
-edit_btn.addEventListener('click', handlerPopupForm);
-cls_btn.addEventListener('click', handleCloseForm);
-
-// Mengaktifkan like
-const like_btn = document.querySelectorAll('.photo-grid__like-btn');
-
-function handlerLikeAct(event) {
-   event.preventDefault();
-
-   if (this.textContent === '\u2665') {
-     this.textContent = '\u2661';
-   } else {
-     this.textContent = '\u2665';
-   }
-}
-
-for (let i = 0; i < like_btn.length; i++) {
-  like_btn[i].addEventListener('click', handlerLikeAct);
-}
-
-// Function input
-const form = popup.querySelector('.popup__container')
-
-function handleProfileFormSubmit(event) {
-  event.preventDefault();
-
-  let inputName = popup.querySelector('.popup__input_text_name')
-  let inputTitle = popup.querySelector('.popup__input_text_title')
-
-  let nameValue = inputName.value;
-  let titleValue = inputTitle.value;
-
-  let dName = document.querySelector('.profile__name')
-  let dTitle = document.querySelector('.profile__title')
-
-  dName.textContent = nameValue;
-  dTitle.textContent = titleValue;
 
   popup.classList.remove('popup_opened');
 }
 
+// Function input
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+
+  let nameValue = inputName.value;
+  let titleValue = inputTitle.value;
+
+  dName.textContent = nameValue;
+  dTitle.textContent = titleValue;
+
+  handleCloseForm(event);
+}
+
+editButton.addEventListener('click', handlerPopupForm);
+closeButton.addEventListener('click', handleCloseForm);
 form.addEventListener('submit', handleProfileFormSubmit);
